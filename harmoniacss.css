990ba9e5c17d6b8db15309dc6c0983a94f8b6bc2
vanillun <37316313+vanillun@users.noreply.github.com>
2018-03-12 21:13:47 -0400

    harmonia.css

---
+++ harmoniacss.css
@@ -0,0 +1,135 @@
+
+<style type="text/CSS">
+
+.blank {
+}
+
+.monajlynntabimage {
+background: url('https://img00.deviantart.net/8672/i/2018/045/a/8/commission___harmonia_by_sealoon-dc33wtk.png');
+background-repeat:no-repeat;
+   background-cover:90%;
+background-position: -50px -100px;
+height:509px;
+width:560px;
+opacity: 1;
+border:3px #fff solid;
+  outline:1px #000 solid;
+}
+
+.monatabhoverjlynn {
+opacity:0;
+width:560px;
+  margin-top:-250px;
+  height:509px;
+      transition:1s;
+  background-image:url('https://img00.deviantart.net/8672/i/2018/045/a/8/commission___harmonia_by_sealoon-dc33wtk.png');
+  background-repeat:no-repeat;
+   background-cover:90%;
+background-position: -50px -100px;
+  color: #a19896;
+text-align:justify;
+ 
+}
+
+.monajlynntabimage:hover .monatabhoverjlynn {
+  opacity:1;
+        transition:1s;
+}
+
+
+.monatabtextjlynn {
+float:left;
+opacity: 0;
+background-color: #111;
+transition:0.9s;
+height:200px;
+  padding:15px;
+overflow:auto;
+color: #656c64;
+font-family:garamond;
+font-size:11px;
+text-align:justify;
+word-spacing:3px;
+letter-spacing:1.5px;
+text-align:justify;
+line-height:12px;
+  margin-top:-142px;
+width:530px;
+
+}
+
+
+.monatabtitlejlynn {
+    opacity:1;
+font-family:garamond;
+    font-size:30px;
+    font-style:itaic;
+  text-transform:uppercase;
+ color:#fff;
+    letter-spacing:10px;
+    text-shadow: 2px 0px #000;
+    text-align:center;
+    line-height:19px;
+    transition:0.7s;
+  padding-top:230px;
+}
+
+.monajlynntabimage:hover .monatabtitlejlynn {
+  opacity:0;
+      transition:0.7s;
+}
+
+
+
+.monatabnamejlynn {
+    opacity:0.8;
+font-family:arial;
+  width:360px;
+  border-top:1px solid #fff;
+    font-size:8px;
+    font-style:itaic;
+  text-transform:uppercase;
+    color:#fff;
+  float:left;
+    letter-spacing:17px;
+    text-shadow: 1px 1px 1px #000;
+    line-height:25px;
+    transition:0.7s;
+  text-align:center;
+  margin-top:5px;
+margin-left:90px;
+  padding-left:14px;
+
+}
+
+.monajlynntabimage:hover .monatabnamejlynn {
+  opacity:0;
+      transition:0.7s;
+}
+
+
+.monajlynntabimage:hover .monatabtextjlynn {
+background-color: #111;
+opacity:0.9;
+transition:0.9s;
+height:200px;
+  padding:15px;
+overflow:auto;
+color: #656c64;
+font-family:garamond;
+font-size:11px;
+text-align:justify;
+word-spacing:3px;
+letter-spacing:1.5px;
+text-align:justify;
+line-height:12px;
+width:530px;
+
+  margin-top:-142px;
+}
+
+
+.monaspeakingjlynn {
+  font-family:garamond;font-size:14px;color:#3eab30;font-style:italic;letter-spacing:2px;font-weight:normal;text-align:left; text-shadow: 0px 0px 10px #222;
+}
+</style>
